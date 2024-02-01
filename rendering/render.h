08e0f662d9fcca97829116bf7366e2c3cdb7c297
elo <orion.at.dawn@gmail.com>
2024-02-01 13:14:39 -0500

    definitions

---
+++ rendering/render.h
@@ -0,0 +1,13 @@
+#ifndef RENDER_H
+# define RENDER_H
+
+# include "../include/structure.h"
+# include <math.h>
+
+//DVCT stands for direction vector
+//CP stands for camera plane
+# define P_SPEED 0.5
+# define DVCT_LEN 1
+# define CP_LEN 1
+
+#endif
